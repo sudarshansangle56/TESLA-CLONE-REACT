ed273aef62b60baa6c5e2f97e0eec09aae9f1e9d
Sudarshan Sangle <142921948+sudarshansangle56@users.noreply.github.com>
2024-09-20 19:27:46 +0530

    Update App.js

--- tcar/src/App.js
+++ tcar/src/App.js
@@ -6,12 +6,9 @@ import Shop from './Pages/Shop';
 import Energy from './Pages/Energy';
 import Vechical from './Pages/Vechical';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Demo from "./Comp/Demo";
 import Custom from "./Comp/Custom";
-
-
-
 
 function App() {
   return (
@@ -23,8 +20,8 @@ function App() {
           <Route path="/Charg" element={<Charg />} />
           <Route path="/Vechical" element={<Vechical />} />
           <Route path="/Energy" element={<Energy />} />
-          <Route path="/Demo" element={<Demo/>} />
-          <Route path="/Custom" element={<Custom/>} />
+          <Route path="/Demo" element={<Demo />} />
+          <Route path="/Custom" element={<Custom />} />
         </Routes>
       </Router>
     </div>
